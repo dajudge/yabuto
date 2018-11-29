9ccc5dc2305e4ffe13c256248e10d27a75dd66a6
Alex Stockinger <mail@alexstockinger.de>
2018-11-29 23:05:55 +0100

    Fixing bintray credentials pipe-through

--- .docker/ci.sh
+++ .docker/ci.sh
@@ -4,11 +4,11 @@ echo "CI_PROJECT_DIR: ${CI_PROJECT_DIR}"
 
 docker run --rm \
     --net host \
-    -e BINTRAY_API_USER:${BINTRAY_API_USER} \
-    -e BINTRAY_API_KEY:${BINTRAY_API_KEY} \
+    -e BINTRAY_API_USER="${BINTRAY_API_USER}" \
+    -e BINTRAY_API_KEY="${BINTRAY_API_KEY}" \
     -v "${CI_PROJECT_DIR}":/project \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "${CI_PROJECT_DIR}/.cache/.m2":/root/.m2 \
     --workdir /project \
-    $(docker build -qt builder_jdk8 ${CI_PROJECT_DIR}/.docker/builder) \
+    $(docker build -q ${CI_PROJECT_DIR}/.docker/builder) \
     $@

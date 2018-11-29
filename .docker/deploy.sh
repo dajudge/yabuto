853903104d117a5f2f44e8953ec8d4795ff7e589
Alex Stockinger <mail@alexstockinger.de>
2018-11-29 22:16:57 +0100

    Multi-stage, removed dependency on bash

--- .docker/deploy.sh
+++ .docker/deploy.sh
@@ -1,15 +1,21 @@
-#! /bin/bash
+#! /bin/sh
 
 BRANCH_NAME=`git branch | grep \* | cut -d ' ' -f2`
 echo "Branch name: ${BRANCH_NAME}"
-VERSION=`echo $BRANCH_NAME | cut -d '/' -f 2`
-echo "Version: ${VERSION}"
 
-if  [[ $BRANCH_NAME == release/* ]] ;
+case "${BRANCH_NAME}" in
+    srelease/*) RELEASE=true ;;
+    *)  RELEASE=false ;;
+esac
+
+if [ $RELEASE = true ] ;
 then
+    VERSION=`echo $BRANCH_NAME | cut -d '/' -f 2`
+    echo "Version: ${VERSION}"
+
     echo "Setting version in pom.xml..."
-    echo "Publishing to bintray..."
     ${CI_PROJECT_DIR}/.docker/ci.sh mvn -s /settings.xml versions:set -DnewVersion=${VERSION}
+    echo "Publishing to bintray..."
     ${CI_PROJECT_DIR}/.docker/ci.sh mvn -s /settings.xml clean install deploy -DskipTests
 else
     echo "Not publishing to bintray."

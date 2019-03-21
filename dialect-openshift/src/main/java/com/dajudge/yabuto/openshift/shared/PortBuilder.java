4b1d0b15277464cc00b7726dc42e718a308154e3
Alex Stockinger <mail@alexstockinger.de>
2019-03-21 09:11:25 +0100

    Allow names for ports

--- dialect-openshift/src/main/java/com/dajudge/yabuto/openshift/shared/PortBuilder.java
+++ dialect-openshift/src/main/java/com/dajudge/yabuto/openshift/shared/PortBuilder.java
@@ -5,6 +5,7 @@ import com.dajudge.yabuto.api.util.ObjectBuilder;
 public class PortBuilder extends ObjectBuilder<PortBuilder> {
     public PortBuilder() {
         me()
+                .simpleValue("name", "name", null, String.class)
                 .simpleValue("port", "port", null, int.class)
                 .simpleValue("containerPort", "containerPort", null, int.class)
                 .simpleValue("targetPort", "targetPort", null, int.class);
